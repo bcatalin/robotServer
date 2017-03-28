793d45fe60adb2a86cebc4f309cb5c1dd2d13d01
root <root@crescatorii.ro>
2017-03-28 12:52:56 +0000

    form

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ var kz = new KalmanFilter();
 
 
 var webConnections = [];
+var ramConnections = [];
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/myappdatabase');
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/myappdatabase');
 
 app.listen(1234);
 
@@ -60,60 +61,19 @@ function handler (req, response)
   });
 
 
-  //console.log(req)
-  // fs.readFile(__dirname + '/index.html',
-  // function (err, data) {
-  //   if (err) {
-  //     res.writeHead(500);
-  //     return res.end('Error loading index.html');
-  //   }
+  if(req.method.toLowerCase() == 'post' )
+  {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) 
+    {
+      console.log("received form");  
+      console.log(fields);
+    });
+  }
 
-  //   res.writeHead(200);
-  //   res.end(data);
-  // });
+
 }
 
-
-function handler2(request, res)
-{
-console.log('request starting...');
-  
-  var filePath = '.' + request.url;
-  if (filePath == './')
-    filePath = './index.htm';
-    
-  var extname = path.extname(filePath);
-  var contentType = 'text/html';
-  switch (extname) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-  }
-  
-  path.exists(filePath, function(exists) {
-  
-    if (exists) {
-      fs.readFile(filePath, function(error, content) {
-        if (error) {
-          response.writeHead(500);
-          response.end();
-        }
-        else {
-          response.writeHead(200, { 'Content-Type': contentType });
-          response.end(content, 'utf-8');
-        }
-      });
-    }
-    else {
-      response.writeHead(404);
-      response.end();
-    }
-  });
-  
-  }  
 
 
 function ParseJson(jsondata) {
@@ -132,63 +92,114 @@ function sendLight(data) {
     io.sockets.emit('light', { light: data.message });
 }
 
+function printRAM()
+{
+ console.log("=======================================================");
+ console.log("|ID| RAM ID     | Socket ID            | Machine ID ");
+ console.log("=======================================================");
+ for(var i=0 ; i < ramConnections.length; i++)
+ {
+  
+  console.log("| "+ i + "| " + ramConnections[i].ram_id + " | " + ramConnections[i].socket_id + " | " + ramConnections[i].machine_id);
+  console.log("-------------------------------------------------------");
+ }
+ console.log("=======================================================");
+}
+//Just in case
+function deleteRAM(socket_id)
+{
+  for(var i=0; i < ramConnections.length; i++)
+  {
+    if(ramConnections[i].socket_id == socket_id)
+    {
+      //remove this element from ramConnections
+    }
+  }//end for
+}
 
-// var nsp = io.of('RBT_81C996');
-// nsp.on('connection', function(socket){
-//   console.log('someone connected in namepace RBT_81C996');
-//   console.log(socket.nsp.name);
-//   nsp.emit('hi', 'Hello everyone!');
-// });
-
-
-// nsp.on('RBT_81C996/JSON', function (data) 
-// {
-//     console.log(data);
-//     //io.sockets.emit('acc_data', { acc_data: data });
-//     nsp.emit('acc_data', { acc_data: data });
-// });
-
-// var nsp2 = io.of('RBT_D232B4');
-// nsp2.on('connection', function(socket){
-//   console.log('someone connected in namepace RBT_D232B4');
-//   console.log(socket.nsp.name);
-//   nsp2.emit('hi', 'Hello everyone!');
-
-// });
-
+function addRAM(obj)
+{
+  for(var i=0; i < ramConnections.length; i++)
+  {
+    if(ramConnections[i].ram_id == obj.ram_id)
+    {
+       ramConnections[i].socket_id = obj.socket_id;
+       ramConnections[i].machine_id = obj.machine_id; //maybe is changed
+       return;
+    }    
+  }
+  ramConnections.push(obj);
+}
 
 io.on('connection', function (socket) 
 {
-	console.log("Connected  socket id:" + socket.id);
-  console.log(socket.handshake.headers.origin);
+  console.log("Connected  socket id:" + socket.id);
+  //console.log(socket.handshake.headers.origin);
   
   if(socket.handshake.headers.origin != "ESP_RAM")
   {
     //possible web browser connection. ESP has Origin preset to ESP_RAM
     webConnections.push(socket);
+    console.log("SOCK: connection - new WEB connection");
+
+  }
+  else
+  {
+    // here the connections from the ESP_RAM are received.
+    // add socket from ESP to the list of RAMs
+    //ramConnections.push(socket);
+    console.log("SOCK: connection - new RAM connection");
   }
 
   socket.emit('welcome', { message: 'Connected !!!!' });
   
   socket.on('connection', function (data) 
   {
-    console.log(data);   
+    console.log("Receive =connection=> from RAM"); console.log(socket.id);
+    //console.log(data);   
+    console.log(JSON.stringify(data));
+
+    var new_ram = new Object();
+    new_ram.socket_id = socket.id;
+    var data_json = ParseJson(JSON.stringify(data)); 
+    //console.log(ParseJson(JSON.stringify(data)));
+    new_ram.ram_id = data_json.ram_id; //console.log(new_ram.ram_id);
+    new_ram.machine_id = data_json.machine_id
+    addRAM(new_ram);
+printRAM();
+    //new connection from a RAM is received in here. Need to send a message
+    // to the connected clients to signal that a new RAM is up and running and
+    // data may apper 
+    for(var webBrowsers = 0; webBrowsers < webConnections.length; webBrowsers++)
+    {
+      console.log("SOCK: RX connection => Send new_ram with data");
+      var sessionID = webConnections[webBrowsers];
+      sessionID.emit('new_ram', { new_ram: data })
+    }
+
   });
   
+  socket.on('hb', function (data) {
+     //sendTime();
+     console.log("app.js - hb function"); console.log(data);
+  });
+
   socket.on('atime', function (data) {
-	  sendTime();
-    console.log(data);
-    });
+     sendTime();
+     console.log("app.js - sendTime function"); console.log(data);
+  });
+
+
   
-  socket.on('light', function (data) {
+  //socket.on('light', function (data) {
     //console.log("light received on SERVER from ESP")
     //sendLight(data);
     //console.log(data);
-    });
+   // });
 
   socket.on('JSON', function (data) 
   {
-  	//console.log(data.x);
+    //console.log(data.x);
     //console.log(kf.filter(data.x))
     // data.x = kx.filter(data.x);
     // data.y = ky.filter(data.y);
@@ -215,7 +226,7 @@ io.on('connection', function (socket)
 
   socket.on('arduino', function (data) 
   {
-	  io.sockets.emit('arduino', { message: 'R0' });
+    io.sockets.emit('arduino', { message: 'R0' });
     console.log(data);
   });
 
@@ -228,7 +239,7 @@ io.on('connection', function (socket)
   });
 
   socket.on("disconnect" , function(reason) {
-   //console.log(socket)
+   console.log(socket.id)
    if(reason == "transport close")
    {
      console.log("receive a disconnect")
---
+++ js/ram.js
@@ -0,0 +1,173 @@
+
+/*****************************
+* add HTML code for a RAM
+******************************/
+function addRam(ram_id)
+{
+      console.log("=======> addRam =====");  
+      var main_div = document.getElementById("main_id");
+           //<table style="width: 808px; height: 212px;" border="1" id="RAM_EC12345">
+           var ram_table = document.createElement("table");
+               ram_table.setAttribute("style", "width: 808px; height: 212px;");
+               ram_table.setAttribute("id", ram_id);
+               ram_table.setAttribute("border", "1");
+             //<tbody>
+             var tbody = document.createElement("tbody");
+                 
+//ROW1
+             //<tr>
+             var tr_1 = document.createElement("tr");
+             //<td style="width: 800px;" rowspan="5" id="td_canvas">
+             var td_canvas = document.createElement("td");  
+                 td_canvas.setAttribute("style","width: 800px;"); 
+                 td_canvas.setAttribute("id","td_canvas");
+                 td_canvas.setAttribute("rowspan","5");
+                 
+            //<canvas id="canvas_RAM_EC12345" width="800" height="200"></canvas><br><br>
+            var ram_canvas = document.createElement("canvas"); 
+                ram_canvas.setAttribute("id","canvas_"+ram_id);
+                ram_canvas.setAttribute("width","800px");
+                ram_canvas.setAttribute("height","200px");
+
+
+             //<td style="width: 406px;" colspan="3" id="td_ram_name" align="center"> RAM_EC12345 - 34,5C</td>
+             var td_title = document.createElement("td");
+                 td_title.setAttribute("width","406px");
+                 td_title.setAttribute("colspan","3");
+                 td_title.setAttribute("id","td_ram_name");
+                 td_title.setAttribute("align","center");
+                 td_title.innerHTML = ram_id;
+//ROW2  X           
+             //<tr> 
+             var tr_2 = document.createElement("tr");
+             //<td style="width: 10px;">X</td>
+             var td_x = document.createElement("td");
+                 td_x.setAttribute("style","10px");
+                 td_x.innerHTML = "X";
+             //<td style="width: 198px;">
+             var td_setX = document.createElement("td");
+                 td_setX.setAttribute("style","width: 198px");
+             //<input type="number" name="setX" size="6" id="RAM_1234_setX">
+             var td_setX_input = document.createElement("input");
+                 td_setX_input.setAttribute("type","number");
+                 td_setX_input.setAttribute("name","setX");
+                 td_setX_input.setAttribute("size","6");
+                 td_setX_input.setAttribute("id",ram_id + "_setX");
+             //<td style="width: 198px;">
+             var td_valueX = document.createElement("td");
+                 td_valueX.setAttribute("style","width: 198px;");                 
+             //<input type="number" name="valX" readonly>
+             var td_viewX_input = document.createElement("input");
+                 td_viewX_input.setAttribute("type", "number");
+                 td_viewX_input.setAttribute("name", "valX");
+                 td_viewX_input.setAttribute("readonly", "true");
+
+//ROW3  Y           
+             //<tr> 
+             var tr_3 = document.createElement("tr");
+             //<td style="width: 10px;">Y</td>
+             var td_y = document.createElement("td");
+                 td_y.setAttribute("style","10px");
+                 td_y.innerHTML = "Y";
+             //<td style="width: 198px;">
+             var td_setY = document.createElement("td");
+                 td_setY.setAttribute("style","width: 198px");
+             //<input type="number" name="setY" size="6" id="RAM_1234_setY">
+             var td_setY_input = document.createElement("input");
+                 td_setY_input.setAttribute("type","number");
+                 td_setY_input.setAttribute("name","setY");
+                 td_setY_input.setAttribute("size","6");
+                 td_setY_input.setAttribute("id",ram_id + "_setY");
+             //<td style="width: 198px;">
+             var td_valueY = document.createElement("td");
+                 td_valueY.setAttribute("style","width: 198px;");                 
+             //<input type="number" name="valX" readonly>
+             var td_viewY_input = document.createElement("input");
+                 td_viewY_input.setAttribute("type", "number");
+                 td_viewY_input.setAttribute("name", "valY");
+                 td_viewY_input.setAttribute("readonly", "true");
+
+//ROW4  Z           
+             //<tr> 
+             var tr_4 = document.createElement("tr");
+             //<td style="width: 10px;">Z</td>
+             var td_z = document.createElement("td");
+                 td_z.setAttribute("style","10px");
+                 td_z.innerHTML = "Z";
+             //<td style="width: 198px;">
+             var td_setZ = document.createElement("td");
+                 td_setZ.setAttribute("style","width: 198px");
+             //<input type="number" name="setZ" size="6" id="RAM_1234_setY">
+             var td_setZ_input = document.createElement("input");
+                 td_setZ_input.setAttribute("type","number");
+                 td_setZ_input.setAttribute("name","setZ");
+                 td_setZ_input.setAttribute("size","6");
+                 td_setZ_input.setAttribute("id",ram_id + "_setZ");
+             //<td style="width: 198px;">
+             var td_valueZ = document.createElement("td");
+                 td_valueZ.setAttribute("style","width: 198px;");                 
+             //<input type="number" name="valX" readonly>
+             var td_viewZ_input = document.createElement("input");
+                 td_viewZ_input.setAttribute("type", "number");
+                 td_viewZ_input.setAttribute("name", "valY");
+                 td_viewZ_input.setAttribute("readonly", "true");
+             
+            
+            //<tr>
+            var tr_5 = document.createElement("tr");
+            //<td style="width: 406px;" colspan="3" align="center">
+            var td_submit = document.createElement("td");
+                td_submit.setAttribute("style","width: 406px;");
+                td_submit.setAttribute("colspan","3");
+                td_submit.setAttribute("align","center");
+            //<form>
+            var td_submit_form = document.createElement("form");
+                td_submit_form.setAttribute("action","");
+                td_submit_form.setAttribute("method","post");
+                td_submit_form.setAttribute("enctype","multipart/form-data");
+            //<button type="submit">
+            var td_submit_form_button = document.createElement("button");
+                td_submit_form_button.setAttribute("type","submit");
+                td_submit_form_button.setAttribute("name","apply");
+                td_submit_form_button.innerHTML = "Apply"
+            
+
+            //tr1,2,3,4
+            tr_1.appendChild(td_canvas); td_canvas.appendChild(ram_canvas);
+            tr_1.appendChild(td_title);
+            tbody.appendChild(tr_1);
+
+            tr_2.appendChild(td_x);
+            tr_2.appendChild(td_setX); 
+              td_setX.appendChild(td_setX_input);
+              td_setX.appendChild(td_setX_input);
+            tr_2.appendChild(td_valueX);
+              td_valueX.appendChild(td_viewX_input);
+            tbody.appendChild(tr_2);
+
+            tr_3.appendChild(td_y);
+            tr_3.appendChild(td_setY); 
+              td_setY.appendChild(td_setY_input);
+              td_setY.appendChild(td_setY_input);
+            tr_3.appendChild(td_valueY);
+              td_valueY.appendChild(td_viewY_input);
+            tbody.appendChild(tr_3);
+            
+            tr_4.appendChild(td_z);
+            tr_4.appendChild(td_setZ); 
+              td_setZ.appendChild(td_setZ_input);
+              td_setZ.appendChild(td_setZ_input);
+            tr_4.appendChild(td_valueZ);
+              td_valueZ.appendChild(td_viewZ_input);
+            tbody.appendChild(tr_4);
+            
+            td_submit_form.appendChild(td_submit_form_button);
+            td_submit.appendChild(td_submit_form);
+            tr_5.appendChild(td_submit);
+            tbody.appendChild(tr_5);
+
+
+            ram_table.appendChild(tbody);
+            main_div.appendChild(ram_table);
+
+    }//end addRAM
